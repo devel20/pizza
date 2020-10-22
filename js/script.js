988822e17437b4aabe2e18bd8d83ddde3719f38a
Oleg <67776507+devel20@users.noreply.github.com>
2020-10-22 18:26:57 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// burger **************************************************
 document.getElementById("header__burger").onclick = function() {
   open()
 };
@@ -7,3 +8,40 @@ function open() {
   document.getElementById("header__menu").classList.toggle("active");
   document.getElementById("body").classList.toggle("lock");
 }
+
+jQuery(document).ready(function($) {
+  let link = $('a[href]');
+    $(link).click(function(){
+    $('.header__burger, .header__menu').removeClass('active');
+    $('body').removeClass('lock');
+  });
+});
+
+// плвный переход к якорю ***************************************
+$(document).ready(function(){
+$(".header__menu").on("click","a", function (event) {
+//отменяем стандартную обработку нажатия по ссылке
+event.preventDefault();
+
+//забираем идентификатор бока с атрибута href
+var id = $(this).attr('href'),
+
+//узнаем высоту от начала страницы до блока на который ссылается якорь
+top = $(id).offset().top;
+
+//анимируем переход на расстояние - top за 1000 мс
+$('body,html').animate({scrollTop: top}, 1000);
+});
+});
+
+// scroll Top ***************************************************
+jQuery(document).ready(function($) {
+$(window).scroll(function() {
+        if ($(this).scrollTop() > 400) {
+            $('.scroll-top').css({opcity: 1}).fadeIn('slow');
+        } else { $('.scroll-top').stop(true, false).fadeOut('fast'); }
+    });
+    $('.scroll-top').click(function() {
+        $('html, body').stop().animate({scrollTop : 0}, 300);
+    });
+});
